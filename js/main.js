163fc0edb0375205c2c70b53b97eb45037a060e3
Elena Surilova <elen21surilova@gmail.com>
2020-11-15 18:37:38 +0200

    Открытие, закрытие меню в мобильной версии, добавление крестика и закрытие по клику на .content

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-const menuBtn = document.querySelector('.mobile-menu');
+const menuBtn = document.querySelector('.fas');
+const openMenuBtn = document.querySelector('.fa-bars');
+const closeMenuBtn = document.querySelector('.fa-times');
 const menu = document.querySelector('.main-navigation');
+const allDoc = document.querySelector('.content');
 
 menuBtn.addEventListener('click', function () {
-	// console.log('клик')
 	menu.classList.toggle("active");
+	menuBtn.classList.toggle("fa-times");
+});
+
+
+allDoc.addEventListener('click', function () {
+	menu.classList.remove("active");
+	menuBtn.classList.toggle("fa-times");
 });
