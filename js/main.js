b62fa37336d87732464111bc15288732379d237f
Elena Surilova <elen21surilova@gmail.com>
2020-11-16 14:57:06 +0200

    добавление слайдера для секции наши услуги

--- js/main.js
+++ js/main.js
@@ -14,3 +14,42 @@ allDoc.addEventListener('click', function () {
 	menu.classList.remove("active");
 	menuBtn.classList.remove("fa-times");
 });
+
+
+$(function () {
+	$('.services-list').slick({
+		prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="assets/img/svg/prev.svg" alt=""></button>',
+		nextArrow: '<button class="slick-next slick-btn" type="button" style=""><img src="assets/img/svg/next.svg" alt=""></button>',
+		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		infinite: true,
+		slidesToShow: 4,
+		responsive: [
+			{
+					breakpoint: 1024,
+					settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+							dots: false
+					}
+			},
+			{
+					breakpoint: 600,
+					settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+					}
+			},
+			{
+					breakpoint: 480,
+					settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+					}
+			}
+		],
+		lazyLoad: 'ondemand'
+	});
+});
